5498c3505e2c9f6895ad7b9e871e6804fe788246
Fai-Aqlah <faialahmary@gmail.com>
2025-11-12 15:35:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from style import load_style
 if "logged_in" not in st.session_state or st.session_state.logged_in is False:
-    st.switch_page("pages/1_Login.py")
+    st.switch_page("pages/1_Login.")
 
 
 
