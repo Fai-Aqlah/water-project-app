d4b79558cfbae970a53e21858e723058a0c79503
Fai-Aqlah <faialahmary@gmail.com>
2025-11-12 09:00:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -91,9 +91,10 @@ st.markdown("""
 <hr style='margin-top:15px; margin-bottom:10px; border: 1px solid #4CAF50;'>
 
 <div style='text-align:center; color:#156b3a; font-weight:bold; font-size:22px;'>
-    Developed by <b>Fai Aqlah</b> | Ministry of Environment, Water & Agriculture 🌿💧 – Hail Branch
+    Developed by <b style="color:#0277bd;">Fai Aqlah</b> | Ministry of Environment, Water & Agriculture 🌿💧 – Hail Branch
     <br>
     Powered by <b>XGBoost</b> & <b>Streamlit</b>
 </div>
 """, unsafe_allow_html=True)
+
 
