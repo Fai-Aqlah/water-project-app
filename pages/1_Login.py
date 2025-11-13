98e5cdad4ab4a23f0099f097f4d94a07faa2e4f1
Fai-Aqlah <faialahmary@gmail.com>
2025-11-12 16:02:26 -0800

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,7 +1,18 @@
 import streamlit as st
 
-st.title("🔐 Login Page Test")
-st.write("If you see this page, the problem is fixed 🎉")
+st.set_page_config(page_title="Login", page_icon="🔐", layout="centered")
 
-    
-   
+# عنوان
+st.markdown("<h2 style='text-align:center;'>🔐 Login Page</h2>", unsafe_allow_html=True)
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if username == "Fai" and password == "1929837":
+        st.session_state.logged_in = True
+        st.success("Login successful! 🎉")
+        st.experimental_rerun()
+    else:
+        st.error("Wrong username or password ❌")
+
