64ce29d2d60b17ec8e9b9ba4682d6a500ec19fdc
Fai-Aqlah <faialahmary@gmail.com>
2025-11-12 16:01:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ import numpy as np
 from style import load_style
 
 
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if st.session_state.logged_in is False:
+    st.switch_page("pages/1_Login.py")
+
+load_style()
+
+st.markdown("<header>Ministry of Environment, Water & Agriculture 🌿</header>", unsafe_allow_html=True)
+
+
 # Apply CSS style
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
