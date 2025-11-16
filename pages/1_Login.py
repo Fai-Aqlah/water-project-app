e2bb937c04f72b9e0b8b9b3ba50742b395c91ce4
Fai-Aqlah <faialahmary@gmail.com>
2025-11-15 17:46:11 -0800

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import re
+import time
 def load_local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
