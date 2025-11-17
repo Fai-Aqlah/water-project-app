724364250a29062c0cafa24f0fdafa84fac908ee
Fai-Aqlah <faialahmary@gmail.com>
2025-11-17 09:53:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 import streamlit as st
 import numpy as np
 from style import load_style
+import requests
+import datetime
+
+def send_email_alert(consumption_value, change_rate):
+    url = "https://api.emailjs.com/api/v1.0/email/send"
+
+    payload = {
+        "service_id": "service_zC5x7rd",
+        "template_id": "template_default",
+        "user_id": "xxxQFdyP4Oz-xCnPL",
+        "template_params": {
+            "to_email": "faialahmary@gmail.com",
+            "message": f"⚠️ High water usage detected!\nCurrent usage: {consumption_value} L\nIncrease: {change_rate:.1f}%",
+            "date": str(datetime.date.today())
+        }
+    }
+
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.post(url, json=payload, headers=headers)
+    return response.status_code
+
+
+
 
 
 
@@ -109,6 +133,9 @@ elif abs_delta < ABS_TOL or abs(change_rate) < PCT_TOL:
 else:
     if change_rate >= LEAK_PCT:
         st.error(f"🚨 Leak/Extreme overuse detected! +{change_rate:.1f}%. Check the system immediately.")
+                send_email_alert(curr_use, change_rate)
+        st.info("📩 Alert email has been sent.")
+
     elif change_rate >= WARN_PCT:
         st.warning(f"⚠️ High increase (+{change_rate:.1f}%). Please monitor usage.")
     elif change_rate <= -PCT_TOL:
